145165c5948521cc86d82b0597b0c0933c129f71
edivaldo.junior <edivaldo.junior@acct.global>
2022-09-29 01:57:48 -0300

    :sparkles: adding information about the while
    adding information about the while

---
+++ loop_while.py
@@ -0,0 +1,22 @@
+"""
+while expressão booleana:
+    //execução do loop
+    
+o bloco do while sera repetido enquanto a expressão booleana for verdadeira.
+
+expressão boolenaa é toda expressão onde o resultado é verdadeiro ou falso
+por ex:
+num = 5
+num < 5
+true
+"""
+
+numero = 1
+while numero < 10:
+    print(numero)
+    numero = numero + 1
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input("Já acabou jéssica?")
