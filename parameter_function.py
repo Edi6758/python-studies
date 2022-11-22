f9e1f26f36a6a383c5b029edbff52991c96d5a1e
edivaldo.junior <edivaldo.junior@acct.global>
2022-11-22 08:31:24 -0300

    :sparkles: function with parameter
    function with parameter

--- parameter_function.py
+++ parameter_function.py
@@ -14,3 +14,15 @@ def cantar_parabens(aniverssariante):
 #funções podem ter N parâmetros
 def soma(a,b):
     return a+b
+
+#parametros sao variaveis declaradas na definição de uma função
+#argumentos são dados passados durante a execução da função
+
+def multiplicar(a,b): #parametros
+    return a*b
+
+multiplicar(3,5) #argumento
+
+#argumentos nomeados
+def diminuir(a="1", b="2"):
+    return a-b
